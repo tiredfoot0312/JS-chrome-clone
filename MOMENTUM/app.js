6c1f8bf59b08c5678ed7559f1b5a4e95ec9c7637
tiredfoot0312 <chlwotjd2130@gmail.com>
2024-02-12 16:09:37 +0900

    const and let
    const : 상수, 기본적으로 사용
    let : 변수, 업데이트 가능, 필요할 때만 사용
    var : 사용 금지

---
+++ MOMENTUM/app.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 1;
+const myName = "jase";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "jaeseong";
+console.log("your new name is " + myName);
