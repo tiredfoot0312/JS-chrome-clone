d9802c62e38bb2f1e2430e716d39894000fddfef
tiredfoot0312 <chlwotjd2130@gmail.com>
2024-03-01 07:21:16 +0900

    Update app.js

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,18 +1,13 @@
-const me = "Jase";
-const array = [1, 2, 3, "four", true, false, me];
-console.log(array);
-array[2] = "two";
-console.log(array);
-array.push("add");
-console.log(array);
+const age = parseInt(prompt("How old are you"));
 
-// object : value having meaning
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonsName) {
-    console.log("Hello! " + otherPersonsName + " nice to meet you!");
-  },
-};
-
-console.log(player.name);
-player.sayHello("Lynn");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should Exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you wnat.");
+}
